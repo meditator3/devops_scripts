3e3842892aa3d4bcd99069b7e29e3fcd420553f1
Ariel <r_e_l_g@yahoo.com>
2024-03-02 18:02:22 +0200

    Update deployLamp.sh

--- vagrant/deployLamp.sh
+++ vagrant/deployLamp.sh
@@ -4,12 +4,11 @@
 
 
 sudo apt-get update
-sudo apt-get install -y apache2 php php-mbstring php-zip phpunit unzip libapache2-mod-php
+sudo apt-get install -y apache2 php php-mbstring php-zip phpunit unzip libapache2-mod-php deconf-utils
 
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password
- admin'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
 
  sudo apt-get install mysql-server -y
  sudo systemctl start mysql 
