c6683987c63a231d2121d992d68c0eda343045d0
Ariel <r_e_l_g@yahoo.com>
2024-03-02 18:11:25 +0200

    Update createUser.sql

--- vagrant/createUser.sql
+++ vagrant/createUser.sql
@@ -3,7 +3,7 @@ CREATE DATABASE myProject;
 
 CREATE USER 'myProject'@'localhost' IDENTIFIED BY 'mypassword';
 
-GRANT ALL PRIVILIGES ON myProject.* TO 'myProject'@'localhost';
+GRANT ALL PRIVILEGES ON myProject.* TO 'myProject'@'localhost';
 
 FLUSH PRIVILEGES
 
