866fb802f56ab10e3b50f04698d13b7b385da98b
Ariel <r_e_l_g@yahoo.com>
2024-03-03 11:05:08 +0200

    Add files via upload

---
+++ Python/project1/template_literals_format.py
@@ -0,0 +1,9 @@
+age = 24
+print("my age is {0} years old".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}...".format(31, "Jan", "Mar", "Aug", "May", "Jul"))
+
+for i in range(1, 13):
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i**2, i**3))
+
+# spaces out with the :2 or :3 or 4:
