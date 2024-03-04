dc7f950b3ce9adb3ae1c2a470c09045900d861d2
Ariel <r_e_l_g@yahoo.com>
2024-03-04 14:21:24 +0200

    Add files via upload

--- Python/project1/argument_passing.py
+++ Python/project1/argument_passing.py
@@ -7,7 +7,7 @@ print(f"first argument: {sys.argv[0]}")
 # the added argument, "ariel" was added in argv[1] < index 1 will now read the argument passed
 # or else we would get error for no argument in index 1
 
-print(f"second argument: {sys.argv[1]}")
+print(f"second argument: {sys.argv[1]}") # need to run this on console to pass an argument
 # or we can just ask for both of them using [0:] , which means till the end
 print(f"positional argument: {sys.argv[0:]}")
 # then we can write in cmd : python argument_passing.py ariel guez
--- Python/project1/file-reverse-CLI.py
+++ Python/project1/file-reverse-CLI.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python 3.6
 # this is supposed to be a script in linux or cli
+# this can be a script that runs without .py!
+# much like with help/manual like in linux commands
+# that can be utilized like a command inside linux only!
+# this one uses filename, to reverse its lines. that's what the script does
+
 # to just reverse the order of lines via a filename
 
 import argparse # library which can parse arguments
@@ -9,12 +15,12 @@ parser.add_argument('--limit', '-l', type=int, help='the number of lines to read
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0') # adding version number
 args = parser.parse_args()
 print(args)
-
+# can use this file-reverse-CLI.py -h to see the help from the above flags/switches
 #open the file
 # args is the argument of the user, .filename is the name we've set on the first add_arg
 # error handeling with try, like js
 
- with open(args.filename) as f:
+with open(args.filename) as f:
         lines = f.readlines()
         lines.reverse() # reserved function of python
         if args.limit:
