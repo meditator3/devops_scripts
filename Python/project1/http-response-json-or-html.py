d4eb343a72b92de211834e64f6ba6ebe00757871
Ariel <r_e_l_g@yahoo.com>
2024-03-06 15:46:43 +0200

    Add files via upload

--- Python/project1/http-response-json-or-html.py
+++ Python/project1/http-response-json-or-html.py
@@ -12,44 +12,50 @@ parser = ArgumentParser(description='this gets url from user and displays conten
 parser.add_argument('--format', help='input JSON or HTML for formatting display of webpage url, HTML defaults', default='HTML')
 parser.add_argument('--url', help='your url link') # we use just args.url to access values
 args = parser.parse_args()
-#
-#
-# if not args.url:
-#     print("You need to input URL")
-#     sys.exit(1)
-#
-# res = requests.get(args.url)
-# res_json = res.json()
-#if args.format !="JSON":
- #   print(res)
-#else: # will copy content of res to a file, and then translate to json using json package
- #   print(res_json)
+
+
+if not args.url:
+    print("You need to input URL")
+    sys.exit(1)
+
+res = requests.get(args.url)
+
+if args.format !="JSON":
+   print(res.text)
+else: # will copy content of res to a file, and then translate to json using json package
+   try:
+      res_json = res.json() # if not put in this block and won't have JSON will throw an error instantly
+      print(json.dumps(res_json, indent=4))
+   except ValueError:
+#             # In case the response is not in JSON format
+       print("The URL did not return JSON data.")
+
 
 
 # AI SCRIPT
-def fetch_url_data(url, output_format):
-    # Make the HTTP request to the URL
-    response = requests.get(url)
-
-    # Check if the user wants the output in JSON format
-    if output_format.upper() == 'JSON':
-        # Attempt to get JSON data directly
-        try:
-            data = response.json()
-            print(data)
-        except ValueError:
-            # In case the response is not in JSON format
-            print("The URL did not return JSON data.")
-    else:
-        # If the user asks for HTML, or any format other than JSON
-        print(response.text)
-
-if __name__ == "__main__":
-    # Set up argument parsing
-    parser = ArgumentParser(description="Fetch URL content and display in specified format.")
-    parser.add_argument('--url', help="URL to fetch data from", required=True)
-    parser.add_argument('--format', help="Format of output: JSON or HTML", default='HTML')
-    args = parser.parse_args()
-
-    # Fetch and display the URL data in the specified format
-    fetch_url_data(args.url, args.format)
+# def fetch_url_data(url, output_format):
+#     # Make the HTTP request to the URL
+#     response = requests.get(url)
+#
+#     # Check if the user wants the output in JSON format
+#     if output_format.upper() == 'JSON':
+#         # Attempt to get JSON data directly
+#         try:
+#             data = response.json()
+#             print(data)
+#         except ValueError:
+#             # In case the response is not in JSON format
+#             print("The URL did not return JSON data.")
+#     else:
+#         # If the user asks for HTML, or any format other than JSON
+#         print(response.text)
+#
+# if __name__ == "__main__":
+#     # Set up argument parsing
+#     parser = ArgumentParser(description="Fetch URL content and display in specified format.")
+#     parser.add_argument('--url', help="URL to fetch data from", required=True)
+#     parser.add_argument('--format', help="Format of output: JSON or HTML", default='HTML')
+#     args = parser.parse_args()
+#
+#     # Fetch and display the URL data in the specified format
+#     fetch_url_data(args.url, args.format)
