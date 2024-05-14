510baf17ef974813c17b0bb39dff0c3bcdf57403
Ariel <r_e_l_g@yahoo.com>
2024-05-14 19:58:03 +0300

    Update replacement_prac.py

--- Python/replacement_prac.py
+++ Python/replacement_prac.py
@@ -10,6 +10,6 @@ for i in range(1, 13):
     print("No, {0} squared is {1} and cubed is {2}".format(i, i**2, i**3)) # prints values of squared cubed of i
     print()
     print("No, {0:2} squared is {1:3} and cubed is {2}".format(i, i**2, i**3)) # prints the same thing but with space bars according to :number
-
+# the :2, or :3 means how many spacebars per template literals
 days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
 print(days[::5]) # because its not an array, MTWTFSS
