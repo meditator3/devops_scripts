25527357dbf2ab4f63dafef9e4e33136ceed2dac
Ariel <r_e_l_g@yahoo.com>
2025-01-07 15:42:32 +0200

    Create interview1_version_extract_increment.py

---
+++ Python/interview_questions/interview1_version_extract_increment.py
@@ -0,0 +1,29 @@
+# tag : aztek_v_1.0.3.300
+# increment last version number by 1 each call of function
+
+tag = "aztek_v_1.0.3.300"
+# remove . and extract last number
+arr_str = tag.split(".")
+print(tag)
+def extract_v_num(tag):
+    return arr_str[len(arr_str)-1]
+
+tag_num = int(extract_v_num(tag))
+
+#returns incremented version number with full str name
+def increment_version_number_return_full_tag (tag_num, list):
+    tag_num+=1
+    list[len(list)-1] = str(tag_num)
+    new_version = '.'.join(list)
+    tag = new_version
+    return tag
+print(arr_str)
+print("tag:",tag)
+
+print(extract_v_num(tag))
+tag = increment_version_number_return_full_tag(tag_num, arr_str)
+tag_num = int(extract_v_num(tag))
+print("new tag: ",tag)
+tag = increment_version_number_return_full_tag(tag_num, arr_str)
+print("new tag: ",tag)
+
