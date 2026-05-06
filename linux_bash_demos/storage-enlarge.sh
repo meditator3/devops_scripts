741e3d0da0ec9abb883d304ecaacbf4a0255858e
Ariel <r_e_l_g@yahoo.com>
2026-05-06 11:14:38 +0300

    Create storage-enlarge.sh

---
+++ linux_bash_demos/storage-enlarge.sh
@@ -0,0 +1,3 @@
+echo "creating new volume for usage- enlarging storage"
+echo "in virtual box -make sure machine is completely shut down first"
+echo "settings>storage>new hard drive> create virtual hard drive>attach it"
