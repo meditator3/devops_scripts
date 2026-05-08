07577b7b72e1abd0980cd911adf6913b22918158
Ariel <r_e_l_g@yahoo.com>
2026-05-08 14:22:56 +0300

    Update iptables_block.sh

--- linux_bash_demos/iptables_block.sh
+++ linux_bash_demos/iptables_block.sh
@@ -5,7 +5,7 @@ sudo iptables -A INPUT -p tcp --dport 8000 -j DROP
 echo " can be added to that line 10.0.0.0/24 to allow this cidr block access and then"
 echo " iptables -A INPUT -j DROP"
 
-echo" now it will show that it is blocked:"
+echo " now it will show that it is blocked:"
 sudo iptables -L --line-numbers
 echo " we use line numbers to determine which rule to delete so i can open the port again"
 echo "this is bad usage of iptables, can be easily done with ufw"
