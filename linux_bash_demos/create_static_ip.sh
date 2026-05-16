c18a03f59396b49163536ccdc0970311a73e35a1
Ariel <r_e_l_g@yahoo.com>
2026-05-16 09:10:34 +0300

    Update create_static_ip.sh

--- linux_bash_demos/create_static_ip.sh
+++ linux_bash_demos/create_static_ip.sh
@@ -8,13 +8,13 @@ networks:
       dhcp4: false
       addresses: 
         - 192.168.1.235/24
-      gateway4: 198.168.1.1
+      gateway4: 192.168.1.1
       nameservers: 
         addresses:
           - 8.8.8.8
       
       
 version: 2
-
+echo "we define /24 bits to inform the system which network it resides. the boundry of the network 192.168.1.0-255 "
 echo " then apply with sudo netplan apply"
 echo " dhcp4 is method for creating temp ip4 addresses, so it must be set to false, in static sceanrio, like here"
