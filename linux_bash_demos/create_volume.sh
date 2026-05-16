b6c0258cf1a74a41151e57e6b447aa5a93866295
Ariel <r_e_l_g@yahoo.com>
2026-05-16 10:07:26 +0300

    Create create_volume.sh

---
+++ linux_bash_demos/create_volume.sh
@@ -0,0 +1,18 @@
+echo "you attach a volume via vm box - make sure to add to SATA and not the ide - or it'll think its boot and vm wont load!"
+echo "create pv,vg, lv (physical volume > volume group > logical volume(size it and save some)"
+sudo pvcreate /dev/sdc
+sudo pvdisplay 
+sudo vgcreate storg1 /dev/sdc
+sudo vgdisplay
+sudo lvcreate --size 40m storg2
+sudo lvdisplay
+
+
+echo "should see now some name on storg2 lv vol0 or something like that"
+echo " now create the file system because otherwise it wont mount to your folder"
+sudo mkfs.ext4 /dev/storg2/lvol0
+echo "create folder for vol"
+sudo mkdir /mnt/vol2-vol0
+sudo mount /dev/storg2/lvol0 /mnt/vol2-vol0
+
+echo " and now to make it permament in fstab"
